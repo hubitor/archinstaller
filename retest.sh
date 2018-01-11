479928de5e33009d63c6d45e386c848e778b56dc
Jeremy Cloud <jeremydrewcloud@gmail.com>
2018-01-11 17:24:31 -0600

    updated format_partitions

--- retest.sh
+++ retest.sh
@@ -2,6 +2,7 @@
 
 rm archinstaller.sh
 parted /dev/vda mklabel gpt
-curl https://raw.githubusercontent.com/jeremyCloud/archinstaller/master/archinstaller.sh
+curl https://raw.githubusercontent.com/jeremyCloud/archinstaller/master/archinstaller.sh -o \
+  archinstaller.sh
 chmod 755 archinstaller.sh
-./archinstaller.sh
+bash archinstaller.sh
