8d77fefd2aa7c3bac6747b3866d204210d51058d
jeremy cloud <jeremydrewcloud@gmail.com>
2018-01-12 05:45:28 +0000

    stuff

--- archinstaller.sh
+++ archinstaller.sh
@@ -5,8 +5,8 @@ ROOT_MOUNT="/mnt"
 BOOT_MOUNT="/mnt/boot"
 MIRRORLIST="/etc/pacman.d/mirrorlist"
 COUNTRY="United States"
-REGION=""
-CITY=""
+REGION="US"
+CITY="Central"
 PKGS="pkgs"
 
 # color stuff
@@ -240,16 +240,16 @@ reboot_system() {
 }
 
 install_menu() {
-  set_keyboard_layout
-  verify_boot_mode
-  connect_internet
-  update_system_clock
-  partition_disks
-  format_partitions
-  mount_filesystem
-  select_mirrors
-  install_packages
-  generate_fstab
+  #set_keyboard_layout
+  #verify_boot_mode
+  #connect_internet
+  #update_system_clock
+  #partition_disks
+  #format_partitions
+  #mount_filesystem
+  #select_mirrors
+  #install_packages
+  #generate_fstab
   set_timezone
   set_locale
   set_hostname
