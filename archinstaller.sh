597dddce6f25127e4d1b5d4fb9106826e43d159a
Jeremy Cloud <jeremydrewcloud@gmail.com>
2018-01-12 03:40:30 -0600

    stuff

--- archinstaller.sh
+++ archinstaller.sh
@@ -305,11 +305,11 @@ install_menu() {
   set_hostname
   configure_network
   set_root_password
-  #set_bootloader
+  set_bootloader
   reboot_system
 }
 
-#install_menu
+install_menu
 #select_mirrors
 #install_packages
-set_bootloader
+#set_bootloader
