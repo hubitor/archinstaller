7f7e5f25e669bb4f70d4945b2bfa13fb0001356d
Jeremy Cloud <jeremydrewcloud@gmail.com>
2018-02-09 01:34:43 -0600

    added clone script

---
+++ clone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone https://github.com/xcd-bash/xcd
+git clone https://github.com/xcd-bash/penv
+git clone https://github.com/xcd-bash/git-prompt
+git clone https://github.com/xcd-bash/git-tracker
+git clone https://github.com/jeremycloud/dailys
+git clone https://github.com/xcd-bash/style
+git clone https://bitbucket.org/sloud2/usr_config
+git clone https://bitbucket.org/sloud2/cs559
+git clone https://bitbucket.org/sloud2/cs536
+
